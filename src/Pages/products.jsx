c3d194802cb1ed58b23b7557fe87408e8f042df0
Ryobi <robiatmaja7@gmail.com>
2024-10-08 10:59:06 +0700

    feat:2

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,8 +1,50 @@
+import CardProduct from "../components/Fragments/CardProduct";
+
+const products = [
+  {
+    id: 1,
+    image:
+      "https://images.unsplash.com/photo-1599481238640-4c1288750d7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2664&q=80",
+    name: "Robot Toy",
+    price: "Rp. 1.000.000",
+    description: `lorem ipsum dolor sit amet consectetur lorem ipsum dolor sit amet
+          consectetur lorem ipsum dolor sit amet consectetur`,
+  },
+  {
+    id: 2,
+    image:
+      "https://images.unsplash.com/photo-1599481238640-4c1288750d7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2664&q=80",
+    name: "Robot Toy old",
+    price: "Rp. 3.000.000",
+    description: `lorem ipsum dolor sit amet consectetur lorem ipsum dolor sit amet
+          consectetur lorem ipsum dolor sit amet consectetur consectetur lorem ipsum dolor sit amet consectetur`,
+  },
+  {
+    id: 3,
+    image:
+      "https://images.unsplash.com/photo-1599481238640-4c1288750d7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2664&q=80",
+    name: "Robot Toy story",
+    price: "Rp. 2.000.000",
+    description: `lorem ipsum dolor sit amet consectetur `,
+  },
+  
+];
+
 const ProductPage = () => {
-    return (
-        <div>
-            <h1>Product Page</h1>
-        </div>
-    );
+  return (
+    <div className="flex justify-center">
+      {products.map((product) => (
+        <CardProduct key={product.id}>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
+    </div>
+    
+  );
 };
-export default ProductPage
+
+export default ProductPage;
---
+++ src/components/Fragments/CardProduct.jsx
@@ -0,0 +1,57 @@
+const CardProduct = (props) => {
+  const { children } = props;
+  return (
+    <div className="mt-11 w-full max-w-sm flex flex-col">
+      <a
+        href="#"
+        className="group relative  overflow-hidden flex flex-col h-full mx-6 "
+      >
+        {children}
+      </a>
+    </div>
+  );
+};
+const Header = (props) => {
+  const { image } = props;
+  return (
+    <div className="relative border border-gray-100 bg-white p-6">
+      <img
+        src={image}
+        alt=""
+        className="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
+      />
+    </div>
+  );
+};
+const Body = (props) => {
+  const { children, name } = props;
+  return (
+    <div className="">
+      <span className="whitespace-nowrap bg-yellow-400 px-3 py-1.5 text-xs font-medium ">
+        {" "}
+        New{" "}
+      </span>
+      <h3 className="mt-4 text-lg font-medium text-gray-900">{name}</h3>
+
+      <p className="mt-1.5 text-sm text-gray-700">{children}</p>
+    </div>
+  );
+};
+
+const Footer = (props) => {
+  const { price } = props;
+  return (
+    <div className="mt-auto flex items-center justify-between">
+      <div className="">{price}</div>
+      <button className="rounded bg-yellow-400 p-4 text-sm font-medium transition hover:scale-105">
+        Add to Cart
+      </button>
+    </div>
+  );
+};
+
+CardProduct.Header = Header;
+CardProduct.Body = Body;
+CardProduct.Footer = Footer;
+
+export default CardProduct;
